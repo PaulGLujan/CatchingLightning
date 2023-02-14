2d20fcf5f7ad4b68a01355f5d79a861353238f1e
Paul <paulglujan@gmail.com>
2023-02-14 10:22:52 -0800

    not sure about this commit..old

--- Main.ino
+++ Main.ino
@@ -2,13 +2,20 @@
 #include "src/Led/Led.h"
 #include "src/MillisDelay/MillisDelay.h"
 
-#define LED_1_PIN 3
+#define LED_1_PIN 2
+#define LED_2_PIN 3
+#define LED_3_PIN 4
+#define LED_4_PIN 5
+#define LED_5_PIN 6
+#define LED_6_PIN 7
 
 Led led1(LED_1_PIN);
+Led led2(LED_2_PIN);
+
 MillisDelay ledDelay;
 
 void setup() {
-  ledDelay.start(100);
+  ledDelay.start(500);
   led1.on();
 };
 
@@ -17,8 +24,10 @@ void loop() {
     ledDelay.repeat();
     if (led1.isOn()) {
       led1.off();
+      led2.on();
     } else {
       led1.on();
+      led2.off();
     }
   }
 }
